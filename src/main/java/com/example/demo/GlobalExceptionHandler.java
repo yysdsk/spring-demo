6138b40a8212d32778a9ea928bba42515968dc54
Yusuke Yasuda <yusuke.yasuda03@g.softbank.co.jp>
2019-09-25 12:37:46 +0900

    使っていないインジェクションを削除

--- src/main/java/com/example/demo/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
